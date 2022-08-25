f6bb5c0e5ed5c2d80fa9a95554fef851588fcd2c
CesarJZO <cesarjzo@outlook.com>
2022-08-25 14:09:58 -0500

    Bonus challenges completed

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -4,14 +4,19 @@ using UnityEngine.InputSystem;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    public float delay;
+    private float _timer;
 
     // Update is called once per frame
     void Update()
     {
         // On space bar press, send dog
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        if (_timer < 0 && Keyboard.current.spaceKey.wasPressedThisFrame)
         {
+            _timer = delay;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
+
+        _timer -= Time.deltaTime;
     }
 }
--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
 
 public class SpawnManagerX : MonoBehaviour
 {
@@ -11,19 +13,21 @@ public class SpawnManagerX : MonoBehaviour
     private float _startDelay = 1.0f;
     private float _spawnInterval = 2f;
 
-    // Start is called before the first frame update
     private void Start()
     {
-        InvokeRepeating(nameof(SpawnRandomBall), _startDelay, _spawnInterval);
+        SpawnRandomBall();
     }
-
-    // Spawn random ball at random x position at top of play area
-    private void SpawnRandomBall ()
+    
+    private async void SpawnRandomBall()
     {
-        // Generate random ball index and random spawn position
-        var spawnPos = new Vector3(Random.Range(_spawnLimitXLeft, _spawnLimitXRight), _spawnPosY, 0);
-        var i = Random.Range(0, ballPrefabs.Length);
-        // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[i], spawnPos, ballPrefabs[i].transform.rotation);
+        while (Application.isPlaying)
+        {
+            var spawnPos = new Vector3(Random.Range(_spawnLimitXLeft, _spawnLimitXRight), _spawnPosY, 0);
+            var i = Random.Range(0, ballPrefabs.Length);
+            Instantiate(ballPrefabs[i], spawnPos, ballPrefabs[i].transform.rotation);
+
+            var randomTime = Random.Range(3000, 5000);
+            await Task.Delay(randomTime);
+        }
     }
 }
