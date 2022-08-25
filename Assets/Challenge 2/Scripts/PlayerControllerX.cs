a34b666acc7dabb69c5ac9b6b05c16cdc1947cbf
CesarJZO <cesarjzo@outlook.com>
2022-08-25 13:50:27 -0500

    "Play Fetch" challenge completed (not counting the bonus)

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On space bar press, send dog
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float _spawnLimitXLeft = 10f;
+    private float _spawnLimitXRight = 12f;
+    private float _spawnPosY = 0;
+
+    private float _startDelay = 1.0f;
+    private float _spawnInterval = 2f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        InvokeRepeating(nameof(SpawnRandomBall), _startDelay, _spawnInterval);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    private void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        var spawnPos = new Vector3(Random.Range(_spawnLimitXLeft, _spawnLimitXRight), _spawnPosY, 0);
+        var i = Random.Range(0, ballPrefabs.Length);
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[i], spawnPos, ballPrefabs[i].transform.rotation);
+    }
+}
